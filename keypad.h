309c229819dd61480b442d3e4c276f6d6eb0b872
DJsquared93 <djplender@gmail.com>
2024-02-15 12:58:45 -0500

    227-lab6
    227lab6part2

---
+++ keypad.h
@@ -0,0 +1,44 @@
+#ifndef __KEYPAD_H_
+#define __KEYPAD_H_
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "stm32f4xx.h"
+
+/* Macro Function */
+#define BIT(x) 			(uint32_t) (1<<x)
+#define SHIFT(x,y)	(uint32_t) (y<<x)
+
+/* Hardware task specific definitions 
+* Pin configs: PC0-3-> Rows
+							 PC4-6-> Cols
+* Description: Touching Rows and Cols
+identified and printed
+*************************************/
+
+#define PC BIT(2)																	// PortC
+#define R0 (uint32_t) 0														// Row bits 0
+#define R1 (uint32_t) 1														// Row bits 1
+#define R2 (uint32_t) 2														// Row bits 2
+#define R3 (uint32_t) 3														// Row bits 3
+#define C0 (uint32_t) 4														// Col bits 0
+#define C1 (uint32_t) 5														// Col bits 1
+#define C2 (uint32_t) 6														// Col bits 2
+
+/* Problem specific parameters */
+//						N/A
+
+/* Hardware task specific functions
+* Pin configs: PC0-3-> Rows
+							 PC4-6-> Cols
+* Description: Touching Rows and Cols
+identified and printed
+*************************************/
+void Keypad_Init (void);                                // Initialize Functions
+void SysTick_Init(void);                                // Initialize Functions
+void SysTick_Delay(uint16_t delay);                     // Initialize Functions
+void Print_Keys (uint16_t *numptr);                     // Print Keypress
+uint8_t Read_Keypad(uint16_t *numptr);                  // Keypad scan subroutine
+
+#endif /* KEYPAD_H_ */
---
+++ main.c
@@ -0,0 +1,39 @@
+/*******************************************************
+* Name:							Atilla Ozgur Cakmak
+* Course:           EGR 226 - Microcontroller Programming and Applications
+* Project:          Keypad Intro
+* File:             main.c
+* Description:      A for loop assigns cols as GND one by one and manually 
+*										checks if any row is shorted. Read is done over row to 
+*										determine the number pressed
+* Pin Configs:      PC0-3=> Rows
+*                   PC4-6=> Cols
+*********************************************************/
+
+#include "keypad.h"
+
+extern void Keypad_Init (void);                                // Initialize Functions
+extern void SysTick_Init(void);                                // Initialize Functions
+extern void SysTick_Delay(uint16_t delay);                     // Initialize Functions
+extern void Print_Keys (uint16_t *numptr);                     // Print Keypress
+extern uint8_t Read_Keypad(uint16_t *numptr);
+
+int main(void)
+{
+    SysTick_Init();
+    Keypad_Init( );                                     // Call initialization functions for functionality
+    uint16_t num, pressed;                              // num is modified over a pointer
+    num=0;
+    uint16_t *numptr=&num;                              // numptr is defined
+	
+		
+		printf("Enter a five digit numerical PIN code NOW!\n");
+	
+		while(1)
+     {
+         pressed = Read_Keypad(numptr);                 // Call Function to read Keypad
+				 if(pressed)
+         Print_Keys(numptr);
+         SysTick_Delay(10);
+     }
+}
